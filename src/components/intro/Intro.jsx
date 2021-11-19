7a2f8f306d104ab336c562b1e7c9b4cf86285558
SandraMey <sandra.meyniel@gmail.com>
2021-11-19 20:08:31 +0100

    finalisation de l'intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import "./intro.scss";
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react';
+import { CollectionsOutlined } from '@material-ui/icons';
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        console.log(textRef);
+        init(textRef.current, {
+            showCursor:false,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: [" React", " JS", " Scss"]
+        });
+
+    }, [])
+
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/profile.png" alt=""></img>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi ther, I'm</h2>
+                    <h1>Sandra Meyniel</h1>
+                    <h3>Developpeuse web<span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt=""/>
+                </a>
+            </div>
         </div>
     )
 }
