0b3569b199f2fd51171615e44fa12199cfc13e8d
SandraMey <sandra.meyniel@gmail.com>
2021-11-20 17:06:02 +0100

    contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import"./contact.scss"
 
 function Contact() {
+
+    const [message, setMessage] = useState(false);
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        setMessage(true)
+    }
+
     return (
         <div className="contact" id="contact">
-            
+            <div className="left">
+                <img src="assets/shake.svg" alt=""/>
+            </div>
+            <div className="right">
+                <h2>Contact</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="test" placeholder="Email"/>
+                    <textarea> Message</textarea>
+                    <button type="submit">Envoyer</button>
+                    {message && <span>Merci, je vous répond très vite :)</span>}
+                </form>
+            </div>
         </div>
     )
 }
